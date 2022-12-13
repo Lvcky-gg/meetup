327952b542965d03c2a3bd803f3ad994fd617abf
John O'Donnell <johnodonnell1997@icloud.com>
2022-12-12 22:41:04 -0600

    first commit

---
+++ authenticate-me/backend/routes/api/index.js
@@ -0,0 +1,50 @@
+const router = require('express').Router();
+
+const sessionRouter = require('./session.js');
+const usersRouter = require('./users.js');
+// const { setTokenCookie } = require('../../utils/auth.js');
+// const { User } = require('../../db/models');
+const { restoreUser } = require('../../utils/auth.js');
+// const { requireAuth } = require('../../utils/auth.js');
+router.use(restoreUser);
+
+router.use('/session', sessionRouter);
+
+router.use('/users', usersRouter);
+
+router.post('/test', function(req, res) {
+  res.json({ requestBody: req.body });
+});
+
+
+
+
+// router.get(
+//   '/restore-user',
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get(
+//   '/require-auth',
+//   requireAuth,
+//   (req, res) => {
+//     return res.json(req.user);
+//   }
+// );
+
+// router.get('/set-token-cookie', async (_req, res) => {
+//   const user = await User.findOne({
+//       where: {
+//         username: 'Demo-lition'
+//       }
+//     });
+//   setTokenCookie(res, user);
+//   return res.json({ user });
+// });
+
+
+  
+
+module.exports = router;
