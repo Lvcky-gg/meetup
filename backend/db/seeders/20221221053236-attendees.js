624e77bf703969c25cd846e6738ad5219d347f0f
John O'Donnell <johnodonnell1997@icloud.com>
2022-12-21 23:03:10 -0600

    added options

--- backend/db/seeders/20221221053236-attendees.js
+++ backend/db/seeders/20221221053236-attendees.js
@@ -1,5 +1,8 @@
 'use strict';
-const options  = {}
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
