bf99d0791b95970264c6c49f1aed2e414d1cc5cc
John O'Donnell <johnodonnell1997@icloud.com>
2023-01-25 22:47:57 -0600

    delete session

--- frontend/src/store/session.js
+++ frontend/src/store/session.js
@@ -56,6 +56,14 @@ export const restoreUser = () => async dispatch => {
     return response;
   };
 
+  export const logout = () => async (dispatch) => {
+    const response = await csrfFetch('/api/session', {
+      method: 'DELETE',
+    });
+    dispatch(removeUser());
+    return response;
+  };
+
 
 const initialState = { user: null };
 
